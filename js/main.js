0262491d1e4ac75f24e13fc1f2340c3ca84eaaf2
Maria Cuello <cuello510510@gmail.com>
2023-06-13 09:07:45 -0300

    entregar

--- js/main.js
+++ js/main.js
@@ -2,8 +2,5 @@ document.addEventListener('DOMContentLoaded', () => {
 	const imgLightBox = document.querySelectorAll('.materialboxed');
 	M.Materialbox.init(imgLightBox, {
 		inDuration: 500,
-		outDuration: 500
-	});
+		outDuration: 500,	});
 });
-
-
